7364f9383861791ffb7f8c735f7fd02117e607fe
MrCano369x <canorueda2000@gmail.com>
2022-03-01 17:21:32 -0600

    conjured item added
    Co-authored-by: DiegoSteve <diegosteve123321@gmail.com>
    Co-authored-by: HeikelV <heikelvillar@gmail.com>

---
+++ gilded_rose_spec.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), 'gilded_rose')
+
+describe GildedRose do
+
+  describe "#update_quality" do
+    it "does not change the name" do
+      items = [Item.new("Conjured Mana Cake", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "Conjured Mana Cake"
+    end
+    
+    it "mantain sellIn " do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 0
+        expect(items[0].quality).to eq 80
+      end
+  end
+
+end
